abc0757f1cb7d2f04d8e03e941408b3e59b5c3b0
cher1shRXD <taewoo080401@gmail.com>
2024-12-11 18:33:14 +0900

    feat: hi

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -4,7 +4,7 @@ import styles from './sidebar.module.css'
 
 const SideBar = () => {
   return (
-    <div className={styles.container}>깃허브 웹훅마스터 민재야 안녕 반가워 ㅎㅇㅎㅇㅎㅇ</div>
+    <div className={styles.container}>깃허브 웹훅마스터 민재야 안녕 반가워 hi</div>
   )
 }
 
